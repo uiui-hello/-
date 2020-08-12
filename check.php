d080045ca2e403a89809303e77e33cd27a95d506
Cloud User <centos@ip-172-31-29-232.us-east-2.compute.internal>
2020-08-13 00:12:31 +0900

    initial

---
+++ check.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>次のフォームに必要事項をご記入ください。</p>
+    <form action="" method="post">
+      <dl>
+          <dt>ニックネーム</dt> 
+          <dd></dd>
+          <dt>メールアドレス</dt> 
+          <dd></dd>
+          <dt>パスワード</dt> 
+          <dd>[表示されません]</dd>
+          <dt>写真など</dt>
+          <dd></dd>
+      </dl>
+      <div><a href="index.php?action=rewrite"&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
+      </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+session_start();
+
+    if(!empty($_POST)) {
+      //エラー項目の確認
+      if($_POST['name'] == '') {
+          $error['name'] = 'blank';
+      }
+      if($_POST['email'] == '') {
+          $error['email'] = 'blank';
+      }
+      if(strlen($_POST['password']) < 4) {
+          $error['password'] = 'length';
+      }
+      if($_POST['password'] == '') {
+          $error['password'] = 'blank';
+      }
+      $fileName = $_FILES['image']['name'];
+      if(!empty($fileName)) {
+          $ext = substr($fileName, -3);
+          if($ext != 'jpg' && $ext != 'gif') {
+              $error['image'] = 'type';
+          }
+      }
+
+      if(empty($error)) {
+          // 画像をアップロードする
+          $image = date('YmdHis') . $_FILES['image']['name'];
+          move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' .$image);
+
+          $_SESSION['join'] = $_POST;
+          $_SESSION['join']['image'] = $image;
+          header('Location: check.php');
+          exit();
+      }
+    }
+    ?>
+
+    <p>次のフォームに必要事項をご記入ください。</p>
+    <form action="" method="post" enctype="multipart/form-data">
+      <dl>
+          <dt>ニックネーム<span class="required">必須</span></dt> 
+          <dd><input type="text" name="name" size="35" maxlength"255" 
+          value="<?php echo htmlspecialchars($_POST['name'], ENT_QUOTES); ?>" />
+          <?php if($error['name'] == 'blank'): ?>
+          <p class="error">* ニックネームを入力してください</p>
+          <?php endif; ?>
+          <dt>メールアドレス<span class="required">必須</span></dt> 
+          <dd><input type="text" name="email" size="35" maxlength"255"
+          value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>" />
+          <?php if($error['email'] == 'blank'): ?>
+          <p class="error">* メールアドレスを入力してください</p>
+          <?php endif; ?>
+          <dt>パスワード<span class="required">必須</span></dt> 
+          <dd><input type="password" name="password" size="10" maxlength"20"
+          value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>" />
+          <?php if($error['password'] == 'blank'): ?>
+          <p class="error">* パスワードを入力してください</p>
+          <?php endif; ?>
+          <?php if($error['password'] == 'length'): ?>
+          <p class="error"* パスワードは４文字以上で入力してください</p>
+          <?php endif; ?>
+          </dd>
+          <dt>写真など</dt>
+          <dd><input type="file" name="image" size="35" /></dd>
+          <?php if($error['image'] == 'type'): ?>
+          <p class="error">* 写真などは[.git]または[.jpg]の画像を指定してください</p>
+          <?php endif; ?>
+          <?php if(!empty($error)): ?>
+          <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+          <?php endif; ?>
+      </dl>
+      <div><input type="submit" value="入力内容を確認する" /></div>
+      </form>
+</body>
+</html>
